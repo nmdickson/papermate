98b3ba931ae27711d602a210d04c81650ae15a62
Nolan Dickson <nmdickso@edu.uwaterloo.ca>
2020-08-08 00:20:22 -0400

    add initial detailed view drawing

--- papermate/interface.py
+++ papermate/interface.py
@@ -25,11 +25,29 @@ def draw_listview(window, articles):
         window.addstr(y + 1, x, article.authors[0] + ' et al.', cs.A_DIM)
 
         # TODO use textwrapper as well, to avoid splitting words
-        abs_win = window.subwin(6, width - 30, y + 3, x)
+        abs_win = window.derwin(6, width - 30, y + 3, x)
         abs_win.addstr(0, 0, article.abstract[:400] + '...')
+
+    window.refresh()
 
 
 def draw_detailedview(window, article):
     '''draw the detailed "more" page for a chosen article'''
     window.clear()
-    pass
+
+    window.border()
+
+    height, width = window.getmaxyx()
+
+    x, y = 5, 2
+
+    title_win = window.derwin(2, width - 30, y, x)
+    title_win.addstr(0, 0, article.title, cs.A_BOLD)
+
+    auth_win = window.derwin(4, width - 30, y + 3, x + 5)
+    auth_win.addstr(0, 0, ', '.join(article.authors), cs.A_DIM)
+
+    abs_win = window.derwin(height - 10, width - 30, y + 7, x + 5)
+    abs_win.addstr(0, 0, article.abstract)
+
+    window.refresh()
