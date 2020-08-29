4c2869efddc426bb9682b8d9820192e43d3e7afb
Nolan Dickson <nmdickso@edu.uwaterloo.ca>
2020-08-29 19:44:08 -0400

    add proper wrapping and windows to list view

--- papermate/interface.py
+++ papermate/interface.py
@@ -1,33 +1,69 @@
 import curses as cs
-import logging
 import textwrap as tw
 
 
 def draw_listview(window, articles):
+    # TODO et al. or all authors should be in options
     window.clear()
 
-    height, width = window.getmaxyx()
+    y = 0
+    max_height, max_width = window.getmaxyx()
 
     for ind, article in enumerate(articles):
 
         # TODO the y depends on text wrapping, figure it out
-        x, y = 10, 2 + ind * 10
+        width = max_width - 25
+        x = 10
+        y += 2
+
+        # ----------------------------------------------------------------------
+        # Numeric label
+        # ----------------------------------------------------------------------
 
         marker = f'[{ind}] '
-
         window.addstr(y, x, marker)
-
         x += len(marker)
+        width -= len(marker)
 
-        # TODO et al. or all authors should be in options
-        authors = article.authors[0] + ' et al.'
+        # ----------------------------------------------------------------------
+        # Title
+        # ----------------------------------------------------------------------
+
+        title = tw.wrap(article.title, width)
+
+        title_win = window.derwin(len(title), width, y, x)
+
+        for ind, line in enumerate(title):
+            title_win.addstr(ind, 0, line, cs.A_BOLD)
+
+        # ----------------------------------------------------------------------
+        # Author
+        # ----------------------------------------------------------------------
+
+        y += len(title)
+
+        authors = tw.wrap(article.authors[0] + ' et al.', width)
+
+        auth_win = window.derwin(len(authors), width, y, x)
+
+        for ind, line in enumerate(authors):
+            auth_win.addstr(ind, 0, line, cs.A_DIM)
+
+        # ----------------------------------------------------------------------
+        # Abstract
+        # ----------------------------------------------------------------------
+
+        y += len(authors)
+
         short_abstract = tw.shorten(article.abstract, 400, placeholder='...')
+        short_abstract = tw.wrap(short_abstract, width)
 
-        window.addstr(y, x, article.title, cs.A_BOLD)
-        window.addstr(y + 1, x, authors, cs.A_DIM)
+        abs_win = window.derwin(len(short_abstract), width, y, x)
 
-        abs_win = window.derwin(6, width - 30, y + 2, x)
-        abs_win.addstr(0, 0, short_abstract)
+        for ind, line in enumerate(short_abstract):
+            abs_win.addstr(ind, 0, line)
+
+        y += len(short_abstract)
 
     window.refresh()
 
@@ -38,7 +74,7 @@ def draw_detailedview(window, article):
 
     x, y = 10, 2
     max_height, max_width = window.getmaxyx()
-    width = max_width - 2 * x
+    width = max_width - 25
 
     # ----------------------------------------------------------------------
     # Title
